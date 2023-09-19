00564001cfe90a535e5be5596bd7e931a605dd1f
mahirairgn <mahirairgn@gmail.com>
2023-09-19 15:04:13 +0700

    mengubah nama model

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
-class Product(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
